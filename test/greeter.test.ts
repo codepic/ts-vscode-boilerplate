12b63327b8fd8306e2402c711d4bf32f8ac5f335
janihyytiainen <jani@codepic.net>
2016-02-23 23:06:40 +0200

    Switched to single quotes

--- test/greeter.test.ts
+++ test/greeter.test.ts
@@ -1,18 +1,18 @@
 /// <refrence path="../source/interfaces/interfaces.d.ts" />
 
-import { expect } from "chai";
-import Greeter from "../source/entities/greeter";
+import { expect } from 'chai';
+import Greeter from '../source/entities/greeter';
 
-describe("Greeter Class", () => {
+describe('Greeter Class', () => {
 
-    it("Should set msg when an instance is created", () => {
-        let expected = "world!";
+    it('Should set msg when an instance is created', () => {
+        let expected = 'world!';
         let greater = new Greeter(expected);
         expect(greater.greeting).eql(expected);
     });
 
-    it("Should greet", () => {
-        let greet = "world!";
+    it('Should greet', () => {
+        let greet = 'world!';
         let greater = new Greeter(greet);
         let actual = greater.greet();
         let expected = `Hello, ${greet}`;
